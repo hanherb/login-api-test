4006759958f7554eeac5064683ad4a2a18c5afc2
hanherb <dyo.pyon@gmail.com>
2020-03-31 19:55:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use('/', route);
 
 app.use(express.static(__dirname + '/public',{ redirect : false }));
 
-var server = app.listen(3000, function () {
+var server = app.listen(3001, function () {
   var port = server.address().port;
   var address = server.address().address;
 
